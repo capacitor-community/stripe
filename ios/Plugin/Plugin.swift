a4d8d9394ee4e3931bb2917b6018022f547af957
Ibby Hadeed <ibby@zyra.ca>
2019-11-02 16:06:46 -0400

    init iOS implementation

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -14,4 +14,23 @@ public class StripePlugin: CAPPlugin {
             "value": value
         ])
     }
+
+    @objc func setPublishableKey(_ call: CAPPluginCall) {}
+    @objc func createCardToken(_ call: CAPPluginCall) {}
+    @objc func createBankAccountToken(_ call: CAPPluginCall) {}
+    @objc func confirmPaymentIntent(_ call: CAPPluginCall) {}
+    @objc func startApplePayTransaction(_ call: CAPPluginCall) {}
+    @objc func finalizeApplePayTransaction(_ call: CAPPluginCall) {}
+    @objc func createSourceToken(_ call: CAPPluginCall) {}
+    @objc func createPiiToken(_ call: CAPPluginCall) {}
+    @objc func createAccountToken(_ call: CAPPluginCall) {}
+    @objc func validateCardNumber(_ call: CAPPluginCall) {}
+    @objc func validateExpiryDate(_ call: CAPPluginCall) {}
+    @objc func validateCVC(_ call: CAPPluginCall) {}
+    @objc func identifyCardBrand(_ call: CAPPluginCall) {}
+    @objc func confirmPaymentIntent(_ call: CAPPluginCall) {}
+    @objc func confirmSetupIntent(_ call: CAPPluginCall) {}
+    @objc func customizePaymentAuthUI(_ call: CAPPluginCall) {}
+    @objc func isGooglePayAvailable(_ call: CAPPluginCall) {}
+    @objc func startGooglePayTransaction(_ call: CAPPluginCall) {}
 }
