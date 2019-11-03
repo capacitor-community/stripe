40bddabf8946166668e41adf59d6f66ec3415ecf
Ibby Hadeed <ibby@zyra.ca>
2019-11-03 09:49:22 -0500

    setup wdio for CI testing

--- packages/test/wdio.conf.js
+++ packages/test/wdio.conf.js
@@ -1,7 +1,11 @@
+const TEST_PLATFORM = process.env.TEST_PLATFORM;
+const testAndroid = TEST_PLATFORM === 'android' || TEST_PLATFORM === 'all';
+const testIos = TEST_PLATFORM === 'ios' || TEST_PLATFORM === 'all';
+const CI = process.env.CI === 'true';
+const RD_KEY = process.env.SAUCE_RD_API_KEY;
+
 const config = {
-  singleton: true,
   runner: 'local',
-  port: 4723,
   specs: [
     './tests/*.spec.js',
   ],
@@ -10,7 +14,7 @@ const config = {
   },
   maxInstances: 1,
   capabilities: [],
-  logLevel: 'error',
+  logLevel: 'debug',
   waitforTimeout: 10000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
@@ -25,32 +29,54 @@ const config = {
   },
 };
 
-const TEST_PLATFORM = process.env.TEST_PLATFORM;
-
-const testAndroid = TEST_PLATFORM === 'android' || TEST_PLATFORM === 'all';
-const testIos = TEST_PLATFORM === 'ios' || TEST_PLATFORM === 'all';
-
 if (testAndroid) {
-  config.capabilities.push({
+  const c = {
     platformName: 'Android',
     platformVersion: process.env.ANDROID_PLATFORM_VERSION || '9.0',
-    deviceName: process.env.ANDROID_DEVICE_NAME || 'any',
+    deviceName: process.env.ANDROID_DEVICE_NAME,
     app: process.env.ANDROID_APK_PATH || './android/app/build/outputs/apk/debug/app-debug.apk',
     autoWebview: true,
     autoGrantPermissions: true,
-  });
+    phoneOnly: false,
+    tabletOnly: false,
+    privateDeviceOnly: false,
+    isHeadless: true,
+  };
+
+  if (CI) {
+    c.testobject_api_key = process.env.SAUCE_ANDROID_API_KEY;
+  }
+
+  config.capabilities.push(c);
 }
 
 if (testIos) {
-  config.capabilities.push({
+  const c = {
     platformName: 'iOS',
     platformVersion: process.env.IOS_PLATFORM_VERSION || '11.0',
     deviceName: process.env.IOS_DEVICE_NAME || 'any',
     app: process.env.IOS_IPA_PATH || './app.ipa',
     autoWebview: true,
     autoGrantPermissions: true,
-  });
+    phoneOnly: false,
+    tabletOnly: false,
+    privateDeviceOnly: false,
+    isHeadless: true,
+  };
+
+  if (CI) {
+    c.testobject_api_key = process.env.SAUCE_IOS_API_KEY;
+  }
+
+  config.capabilities.push(c);
 }
 
+if (CI) {
+  config.capabilities.forEach(c => {
+    c.testobject_api_key = RD_KEY;
+  });
+} else {
+  config.port = 4237;
+}
 
 exports.config = config;
