daedcaa6beb3bb84d90a1e91aff82d7c2fe0217e
Ibby Hadeed <ibby@zyra.ca>
2020-06-16 17:00:45 -0400

    restore test to kotlin syntax

--- packages/plugin/android/src/test/java/ca/zyra/capacitor/stripe/ExampleUnitTest.kt
+++ packages/plugin/android/src/test/java/ca/zyra/capacitor/stripe/ExampleUnitTest.kt
@@ -1,17 +1,18 @@
 package ca.zyra.capacitor.stripe
 
-import org.junit.Test;
+import org.junit.Test
 
-import static org.junit.Assert.*;
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-public class ExampleUnitTest {
+class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    @Throws(Exception::class)
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
     }
 }
