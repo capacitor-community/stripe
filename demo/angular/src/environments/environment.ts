44b9dc904d7d45943cb935d4a208709b78348dab
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2021-07-13 09:32:42 +0900

    feat(server): add endpoint 'payment-sheet'

--- demo/angular/src/environments/environment.ts
+++ demo/angular/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: 'https://j3x0ln9gj7.execute-api.ap-northeast-1.amazonaws.com/dev/',
 };
 
 /*
