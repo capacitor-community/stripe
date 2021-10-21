7dd4fa714be40befba2b0eaa0dad156b6daf1d52
Ashish Saluja <ashishsaluja1@gmail.com>
2021-10-21 14:52:53 +0200

    Dummy commit 4

--- android/src/main/java/com/getcapacitor/community/stripe/googlepay/GooglePayExecutor.java
+++ android/src/main/java/com/getcapacitor/community/stripe/googlepay/GooglePayExecutor.java
@@ -23,7 +23,7 @@ public class GooglePayExecutor extends Executor {
     public GooglePayPaymentMethodLauncher googlePayPaymentMethodLauncher;
     private final JSObject emptyObject = new JSObject();
     private String clientSecret;
-    public boolean isAvailable; // Ashu 3
+    public boolean isAvailable; // Ashu 4
 
     public GooglePayExecutor(
             Supplier<Context> contextSupplier,
