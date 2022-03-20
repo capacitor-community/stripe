ced44870218283f2ebac5902cea71492984de577
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2022-03-20 17:10:37 +0900

    feat(applepay): implemets on web

--- src/web.ts
+++ src/web.ts
@@ -12,13 +12,18 @@ import type {
   CreatePaymentFlowOption,
   PaymentFlowResultInterface,
   ApplePayResultInterface,
-  GooglePayResultInterface,
+  GooglePayResultInterface, CreateApplePayOption,
 } from './definitions';
-import { PaymentFlowEventsEnum, PaymentSheetEventsEnum } from './definitions';
+import {ApplePayEventsEnum, PaymentFlowEventsEnum, PaymentSheetEventsEnum} from './definitions';
 import { isPlatform } from './shared/platform';
 
 interface StripePaymentSheet
   extends Components.StripePaymentSheet,
+    HTMLStencilElement,
+    HTMLElement {}
+
+interface StripeRequestButton
+  extends Components.StripePaymentRequestButton,
     HTMLStencilElement,
     HTMLElement {}
 
@@ -28,6 +33,9 @@ export class StripeWeb extends WebPlugin implements StripePlugin {
 
   private flowStripe: Stripe | undefined;
   private flowCardNumberElement: StripeCardNumberElement | undefined;
+
+  private requestButton: StripeRequestButton | undefined;
+  private requestButtonOptions: CreateApplePayOption | undefined;
 
   constructor() {
     super({
@@ -205,17 +213,84 @@ export class StripeWeb extends WebPlugin implements StripePlugin {
   }
 
   isApplePayAvailable(): Promise<void> {
-    throw this.unimplemented('Not implemented on web.');
+    return new Promise(resolve => resolve());
   }
 
-  createApplePay(): Promise<void> {
-    throw this.unimplemented('Not implemented on web.');
+  async createApplePay(createApplePayOption: CreateApplePayOption): Promise<void> {
+    if (!this.publishableKey) {
+      this.notifyListeners(ApplePayEventsEnum.FailedToLoad, null);
+      return;
+    }
+
+    this.requestButton = document.createElement('stripe-payment-request-button');
+    document.querySelector('body')?.appendChild(this.requestButton);
+    await customElements.whenDefined('stripe-payment-request-button');
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      this.requestButton.applicationName = '@capacitor-community/stripe';
+    } catch (e) {
+      console.error(e);
+    }
+
+    this.requestButtonOptions = createApplePayOption;
+    this.notifyListeners(ApplePayEventsEnum.Loaded, null);
   }
 
   presentApplePay(): Promise<{
     paymentResult: ApplePayResultInterface;
   }> {
-    throw this.unimplemented('Not implemented on web.');
+    // eslint-disable-next-line no-async-promise-executor
+    return new Promise(async (resolve) => {
+      if (this.requestButton === undefined || this.requestButtonOptions === undefined || this.publishableKey === undefined) {
+        return resolve({
+          paymentResult: ApplePayEventsEnum.Failed
+        });
+      }
+      await this.requestButton.setPaymentRequestOption({
+        country: this.requestButtonOptions.countryCode.toUpperCase(),
+        currency: this.requestButtonOptions.currency.toLowerCase(),
+        total: this.requestButtonOptions.paymentSummaryItems[this.requestButtonOptions.paymentSummaryItems.length - 1],
+        displayItems: this.requestButtonOptions.paymentSummaryItems,
+      });
+
+      // await this.requestButton.setPaymentRequestShippingAddressEventHandler(async (event, stripe) => {});
+      const intentClientSecret = this.requestButtonOptions.paymentIntentClientSecret;
+      await this.requestButton.setPaymentMethodEventHandler(async (event, stripe) => {
+        const line1: string[] = event?.shippingAddress?.addressLine || [];
+        const { error } = await stripe.confirmCardPayment(
+          intentClientSecret,
+          {
+            payment_method: event.paymentMethod.id,
+            shipping: {
+              name: event?.shippingAddress?.recipient || '',
+              phone: event?.shippingAddress?.phone,
+              address: {
+                line1: line1.length ? line1[0] : '',
+                city: event?.shippingAddress?.city,
+                postal_code: event?.shippingAddress?.postalCode,
+                state: event?.shippingAddress?.region,
+                country: event?.shippingAddress?.country,
+              },
+            },
+          },
+          { handleActions: false }
+        );
+        if (error) {
+          this.notifyListeners(ApplePayEventsEnum.Failed, null);
+          resolve({
+            paymentResult: ApplePayEventsEnum.Failed
+          });
+        }
+        resolve({
+          paymentResult: ApplePayEventsEnum.Completed
+        });
+      });
+      await this.requestButton.initStripe(this.publishableKey);
+
+      // ここでApplePayのクリックイベント
+    });
   }
 
   isGooglePayAvailable(): Promise<void> {
