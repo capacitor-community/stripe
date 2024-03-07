85e43efc12cdb974f4995acd68c3b4b1777a4a1b
榊原昌彦 <sakakibara@rdlabo.jp>
2024-03-07 14:15:49 +0900

    feat(payment): update stripeAdnroidVersion to currently note: v20.37.2 updated dependencies

--- demo/sdk-fixed/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ demo/sdk-fixed/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.community.stripe.StripePlugin;
 import com.getcapacitor.community.stripe.identity.StripeIdentityPlugin;
-import com.getcapacitor.community.stripe.terminal.StripeTerminalPlugin;
 
 public class MainActivity extends BridgeActivity {
 
@@ -14,8 +13,5 @@ public class MainActivity extends BridgeActivity {
         super.onCreate(savedInstanceState);
         registerPlugin(StripePlugin.class);
         registerPlugin(StripeIdentityPlugin.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-          registerPlugin(StripeTerminalPlugin.class);
-        }
     }
 }
