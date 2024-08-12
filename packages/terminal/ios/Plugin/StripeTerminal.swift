422948faa9a498b3473e381b21a9950259f36c15
榊原昌彦 <sakakibara@rdlabo.jp>
2024-08-12 09:32:00 +0900

    fix(terminal): messageType follow Android Type

--- packages/terminal/ios/Plugin/StripeTerminal.swift
+++ packages/terminal/ios/Plugin/StripeTerminal.swift
@@ -250,52 +250,6 @@ public class StripeTerminal: NSObject, DiscoveryDelegate, LocalMobileReaderDeleg
         call.resolve([:])
     }
 
-    private func mapToSimulateReaderUpdate(_ update: String) -> SimulateReaderUpdate {
-        switch update {
-        case "UpdateAvailable": return SimulateReaderUpdate.available
-        case "None": return SimulateReaderUpdate.none
-        case "Random": return SimulateReaderUpdate.random
-        case "Required": return SimulateReaderUpdate.required
-        case "LowBattery": return SimulateReaderUpdate.lowBattery
-        case "LowBatterySucceedConnect": return SimulateReaderUpdate.lowBatterySucceedConnect
-        default: return SimulateReaderUpdate.none
-        }
-    }
-
-    private func mapToCardType(type: String) -> UInt {
-        switch type {
-        case "VISA": return 0
-        case "VISA_DEBIT": return 1
-        case "MASTERCARD": return 3
-        case "MASTERCARD_DEBIT": return 2
-        case "MASTERCARD_PREPAID": return 5
-        case "AMEX": return 6
-        case "AMEX_2": return 7
-        case "DISCOVER": return 8
-        case "DISCOVER_2": return 9
-        case "DINERS": return 10
-        case "DINERS_14_DIGITS": return 11
-        case "JCB": return 12
-        case "UNION_PAY": return 13
-        case "INTERAC": return 14
-        case "EFTPOS_AU_DEBIT": return 15
-        case "VISA_US_COMMON_DEBIT": return 2
-        case "CHARGE_DECLINED": return 18
-        case "CHARGE_DECLINED_INSUFFICIENT_FUNDS": return 19
-        case "CHARGE_DECLINED_LOST_CARD": return 20
-        case "CHARGE_DECLINED_STOLEN_CARD": return 21
-        case "CHARGE_DECLINED_EXPIRED_CARD": return 22
-        case "CHARGE_DECLINED_PROCESSING_ERROR": return 23
-        case "EFTPOS_AU_VISA_DEBIT": return 16
-        case "EFTPOS_AU_DEBIT_MASTERCARD": return 17
-        case "OFFLINE_PIN_CVM": return 27
-        case "OFFLINE_PIN_SCA_RETRY": return 28
-        case "ONLINE_PIN_CVM": return 25
-        case "ONLINE_PIN_SCA_RETRY": return 26
-        default:
-            return 0
-        }
-    }
 
     /*
      * Terminal
@@ -404,8 +358,10 @@ public class StripeTerminal: NSObject, DiscoveryDelegate, LocalMobileReaderDeleg
     }
 
     public func reader(_: Reader, didRequestReaderDisplayMessage displayMessage: ReaderDisplayMessage) {
+        let result = self.mapFromReaderDisplayMessage(displayMessage)
+        
         self.plugin?.notifyListeners(TerminalEvents.RequestDisplayMessage.rawValue, data: [
-            "messageType": displayMessage.rawValue,
+            "messageType": result,
             "message": displayMessage.rawValue
         ])
     }
@@ -435,6 +391,68 @@ public class StripeTerminal: NSObject, DiscoveryDelegate, LocalMobileReaderDeleg
             "requiredAt": update.requiredAt.description,
             "timeEstimate": String(update.estimatedUpdateTime.rawValue)
         ]
+    }
+    
+    private func mapToSimulateReaderUpdate(_ update: String) -> SimulateReaderUpdate {
+        switch update {
+        case "UpdateAvailable": return SimulateReaderUpdate.available
+        case "None": return SimulateReaderUpdate.none
+        case "Random": return SimulateReaderUpdate.random
+        case "Required": return SimulateReaderUpdate.required
+        case "LowBattery": return SimulateReaderUpdate.lowBattery
+        case "LowBatterySucceedConnect": return SimulateReaderUpdate.lowBatterySucceedConnect
+        default: return SimulateReaderUpdate.none
+        }
+    }
+
+    private func mapToCardType(type: String) -> UInt {
+        switch type {
+        case "VISA": return 0
+        case "VISA_DEBIT": return 1
+        case "MASTERCARD": return 3
+        case "MASTERCARD_DEBIT": return 2
+        case "MASTERCARD_PREPAID": return 5
+        case "AMEX": return 6
+        case "AMEX_2": return 7
+        case "DISCOVER": return 8
+        case "DISCOVER_2": return 9
+        case "DINERS": return 10
+        case "DINERS_14_DIGITS": return 11
+        case "JCB": return 12
+        case "UNION_PAY": return 13
+        case "INTERAC": return 14
+        case "EFTPOS_AU_DEBIT": return 15
+        case "VISA_US_COMMON_DEBIT": return 2
+        case "CHARGE_DECLINED": return 18
+        case "CHARGE_DECLINED_INSUFFICIENT_FUNDS": return 19
+        case "CHARGE_DECLINED_LOST_CARD": return 20
+        case "CHARGE_DECLINED_STOLEN_CARD": return 21
+        case "CHARGE_DECLINED_EXPIRED_CARD": return 22
+        case "CHARGE_DECLINED_PROCESSING_ERROR": return 23
+        case "EFTPOS_AU_VISA_DEBIT": return 16
+        case "EFTPOS_AU_DEBIT_MASTERCARD": return 17
+        case "OFFLINE_PIN_CVM": return 27
+        case "OFFLINE_PIN_SCA_RETRY": return 28
+        case "ONLINE_PIN_CVM": return 25
+        case "ONLINE_PIN_SCA_RETRY": return 26
+        default:
+            return 0
+        }
+    }
+    
+    private func mapFromReaderDisplayMessage(_ displayMessage: ReaderDisplayMessage) -> String {
+        switch displayMessage {
+        case ReaderDisplayMessage.insertCard: return "INSERT_CARD"
+        case ReaderDisplayMessage.insertOrSwipeCard: return "INSERT_OR_SWIPE_CARD"
+        case ReaderDisplayMessage.multipleContactlessCardsDetected: return "MULTIPLE_CONTACTLESS_CARDS_DETECTED"
+        case ReaderDisplayMessage.removeCard: return "REMOVE_CARD"
+        case ReaderDisplayMessage.retryCard: return "RETRY_CARD"
+        case ReaderDisplayMessage.swipeCard: return "SWIPE_CARD"
+        case ReaderDisplayMessage.tryAnotherCard: return "TRY_ANOTHER_CARD"
+        case ReaderDisplayMessage.tryAnotherReadMethod: return "TRY_ANOTHER_READ_METHOD"
+        case ReaderDisplayMessage.cardRemovedTooEarly: return "CARD_REMOVED_TOO_EARLY"
+        default: return "unknown"
+        }
     }
 }
 
