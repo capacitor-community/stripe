4c4d7f2234f84f4570f817043c9ae0c16d50a1f3
榊原昌彦 <sakakibara@rdlabo.jp>
2024-12-04 11:07:19 +0900

    fix(terminal): ios require v14

--- packages/terminal/Package.swift
+++ packages/terminal/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorCommunityStripeTerminal",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "CapacitorCommunityStripeTerminal",
