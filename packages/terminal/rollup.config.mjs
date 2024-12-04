1e02a3fec8138bc6ad087d7ed71720795ae90e14
榊原昌彦 <sakakibara@rdlabo.jp>
2024-12-04 09:59:31 +0900

    feat(): update every package dependency

--- packages/terminal/rollup.config.mjs
+++ packages/terminal/rollup.config.mjs
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorStripeIdentity',
+      name: 'capacitorStripe',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
