2270b5258554a4ab15ea0e6cd589d368e82350c8
榊原昌彦 <sakakibara@rdlabo.jp>
2025-06-19 19:16:31 +0900

    feat(packages): update every packages Note: StripePaymentSheet@24.26.0 is broken at payment flow

--- packages/terminal/CapacitorCommunityStripeTerminal.podspec
+++ packages/terminal/CapacitorCommunityStripeTerminal.podspec
@@ -13,6 +13,6 @@ Pod::Spec.new do |s|
   s.source_files = 'ios/Sources/**/*.{swift,h,m,c,cc,mm,cpp}'
   s.ios.deployment_target = '14.0'
   s.dependency 'Capacitor'
-  s.dependency 'StripeTerminal', '~> 4.3.0'
+  s.dependency 'StripeTerminal', '~> 4.5.0'
   s.swift_version = '5.1'
 end
--- packages/terminal/Package.swift
+++ packages/terminal/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
-        .package(url: "https://github.com/stripe/stripe-terminal-ios.git", exact: "4.3.0")
+        .package(url: "https://github.com/stripe/stripe-terminal-ios.git", exact: "4.5.0")
     ],
     targets: [
         .target(
